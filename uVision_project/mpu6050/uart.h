7a6ff6ab152a9f416531c9f7e7f7c12994f9f10c
Michał Kwiatosz <Micha Kwiatosz>
2015-12-15 16:53:16 +0100

    Uart communication ( comments for doxygen )

--- uVision_project/mpu6050/uart.h
+++ uVision_project/mpu6050/uart.h
@@ -1,11 +1,17 @@
+/**
+* @file  uart.h
+*
+* Communication via UART
+*/
+
 #ifndef UART_H
 #define UART_H
 
-/*
+/**
  * For this code to work properly the "CLOCK_SETUP" must be set to 1:
  *   1 ... Multipurpose Clock Generator (MCG) in PLL Engaged External (PEE) mode
  *         Reference clock source for MCG module is an external crystal 8MHz
- *         Core clock = 48MHz, BusClock = 24MHz 
+ *         Core clock = 48MHz, BusClock = 24MHz. 
  */
 
 #include "MKL46Z4.h"
@@ -14,21 +20,42 @@
 #include <cstdarg> // va_list, va_start, va_end
 #include <cstdio> // vsprintf()
 
-#define MAX_BUFF_SIZE 64 // maximum buffer size used for transmission
+/**
+* Maximum buffer size used for transmission.
+*/
+#define MAX_BUFF_SIZE 64 
 
-/* Initializatino function */
+/**
+* Initializatino function.
+*/
 void uart_init(void);
 
-/* Send the data by polling the Transmitter Empty flag */
+/**
+* Send the data by polling the Transmitter Empty flag.
+* @param[in] data 
+*/
 void uart_transmit_poll(uint8_t data);
-/* Receive the data by polling the Receiver Full flag */
+
+/**
+* Receive the data by polling the Receiver Full flag.
+*/
 uint8_t uart_receive_poll(void);
-/* Send the data by interrupt-driven communication */
+
+/**
+* Send the data by interrupt-driven communication.
+* @param[in] data
+*/
 void uart_transmit(const char *data, ...);
-/* Read the data from the queue */
+
+/**
+* Read the data from the queue.
+* @param[in] data
+*/
 void uart_receive(char *data);
 
-/* IRQ Handler for the interrupt-driven communication */
+/**
+* IRQ Handler for the interrupt-driven communication.
+*/
 void UART0_IRQHandler(void);
 
 #endif
