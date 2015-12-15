332578e27f71c94e5ae4e4ddfb4d9e225e528866
Jakub Nawala <jakub.tadeusz.nawala@gmail.com>
2015-12-15 17:15:49 +0100

    Add the comment mentioning about the required clock setup

--- uVision_project/mpu6050/uart.h
+++ uVision_project/mpu6050/uart.h
@@ -1,18 +1,15 @@
 /**
 * @file  uart.h
 *
-* Communication via UART
+* Communication via UART for FRDM-MKL46Z board.
+* For this code to work properly the "CLOCK_SETUP" must be set to 1:\n
+*   1 ... Multipurpose Clock Generator (MCG) in PLL Engaged External (PEE) mode
+*         Reference clock source for MCG module is an external crystal 8MHz
+*         Core clock = 48MHz, BusClock = 24MHz. 
 */
 
 #ifndef UART_H
 #define UART_H
-
-/**
- * For this code to work properly the "CLOCK_SETUP" must be set to 1:
- *   1 ... Multipurpose Clock Generator (MCG) in PLL Engaged External (PEE) mode
- *         Reference clock source for MCG module is an external crystal 8MHz
- *         Core clock = 48MHz, BusClock = 24MHz. 
- */
 
 #include "MKL46Z4.h"
 #include "queue.h"
@@ -21,41 +18,42 @@
 #include <cstdio> // vsprintf()
 
 /**
-* Maximum buffer size used for transmission.
-*/
+ * Maximum buffer size used for transmission.
+ */
 #define MAX_BUFF_SIZE 64 
 
 /**
-* Initializatino function.
-*/
+ * Initializatino function.
+ */
 void uart_init(void);
 
 /**
-* Send the data by polling the Transmitter Empty flag.
-* @param[in] data 
-*/
+ * Send the data by polling the Transmitter Empty flag.
+ * @param[in] data 
+ */
 void uart_transmit_poll(uint8_t data);
 
 /**
-* Receive the data by polling the Receiver Full flag.
-*/
+ * Receive the data by polling the Receiver Full flag.
+ * @return data received from UART communication
+ */
 uint8_t uart_receive_poll(void);
 
 /**
-* Send the data by interrupt-driven communication.
-* @param[in] data
-*/
+ * Send the data by interrupt-driven communication.
+ * @param[in] data
+ */
 void uart_transmit(const char *data, ...);
 
 /**
-* Read the data from the queue.
-* @param[in] data
-*/
+ * Read the data from the UART interface.
+ * @param[out] data buffer in which to store the received data.
+ */
 void uart_receive(char *data);
 
 /**
-* IRQ Handler for the interrupt-driven communication.
-*/
+ * IRQ Handler for the interrupt-driven communication.
+ */
 void UART0_IRQHandler(void);
 
 #endif
