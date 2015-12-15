14a168084586419a9a837710f69bba70a2a5c2c0
Jakub Nawala <jakub.tadeusz.nawala@gmail.com>
2015-12-15 16:53:37 +0100

    Add the doxygen style comments

--- uVision_project/mpu6050/extra.h
+++ uVision_project/mpu6050/extra.h
@@ -1,8 +1,17 @@
+/**
+ * @file extra.h
+ * Library including functions useful in embedded progamming.
+ */
+
 #ifndef extra_h
 #define extra_h
 
 #include "MKL46Z4.h"   /* Device header */	
 
+/**
+ * Delay the operation of the MCU for value*10 000 clock cycles.
+ * @param[in] value number of clock cyckles*10 000 to delay the operation of MCU. 
+ */
 void delay_mc(uint32_t value);
 
 #endif
