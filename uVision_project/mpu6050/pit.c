25f5867a06789bea5d65c2bf8a1c71f604639385
Qub3k <jakub.tadeusz.nawala@gmail.com>
2016-01-11 17:03:05 +0100

    Increase the speed of data transmission to 50 Hz

--- uVision_project/mpu6050/pit.c
+++ uVision_project/mpu6050/pit.c
@@ -9,6 +9,7 @@ float gyro_sens = 0;
 unsigned short accel_sens = 0;
 float tmp_fl = 0;
 short int tmp_sh_int = 0;
+uint32_t timestamp = 0;
 
 void pit_init() {
   /* Initialize the I2C */
@@ -34,7 +35,7 @@ void pit_init() {
   
   // Set period for PIT (keep in mind that Periodic Interrupt Timer is 32-bit long, counts down and operates
   // at frequency equal to 24 MHz (41.67 ns / tic)
-  PIT->CHANNEL[0].LDVAL = 0x00249F00;   // Set period to 0.1 s = 1*24*10^6 tics // 0x16E3600;// Set period to 1 s
+  PIT->CHANNEL[0].LDVAL = 0x75300; // Set period to 0.02s = 50 Hz
   
   // Configure Nested Vectored Interrupt Controller
   NVIC_SetPriority(PIT_IRQ_NBR, 2);
@@ -55,6 +56,10 @@ void pit_init() {
 void PIT_IRQHandler(void) {
   // Check which channel generated an interrupt
   if(PIT->CHANNEL[0].TFLG & PIT_TFLG_TIF_MASK) {
+    /* Increase the timestamp value */
+    timestamp++;
+    
+    /* Send the gyro data */
     if(mpu_get_gyro_reg(gyro_data, (unsigned long *)NULL)){
 			// do nothing if reading failed
     }else{
@@ -67,6 +72,8 @@ void PIT_IRQHandler(void) {
 			//printf("%+08hd\t%+08hd\t%+08hd", gyro_data[0] , gyro_data[1] , gyro_data[2]);
       printf("%+hd\t%+hd\t%+hd", gyro_data[0] , gyro_data[1] , gyro_data[2]);
     }
+    
+    /* Send the accelerometer data */
     if(mpu_get_accel_reg(accel_data, (unsigned long *)NULL)){
       // do nothing if reading failed
     }else{
@@ -77,8 +84,11 @@ void PIT_IRQHandler(void) {
       tmp_fl = (float)accel_data[2]/(float)accel_sens;
       accel_data[2] = tmp_fl*1000;
       //printf("\t%+08hd\t%+08hd\t%+08hd\n", accel_data[0], accel_data[1] , accel_data[2]);
-      printf("\t%+hd\t%+hd\t%+hd\n", accel_data[0], accel_data[1] , accel_data[2]);
+      printf("\t%+hd\t%+hd\t%+hd", accel_data[0], accel_data[1] , accel_data[2]);
     }
+    
+    /* Send the timestamp */
+    printf("\t%u\n", timestamp);
     
     // Clear channel 0 interrupt flag
     PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK;
